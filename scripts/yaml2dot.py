62d5017079a179b9e7353b43b6fe8cb18953327e
Roni Choudhury <roni.choudhury@kitware.com>
2014-12-22 16:53:50 -0500

    Adding a working yaml2dot script

--- scripts/yaml2dot.py
+++ scripts/yaml2dot.py
@@ -2,7 +2,18 @@ import sys
 import yaml
 
 def main():
-    return 0
+    try:
+        maze = yaml.safe_load(sys.stdin.read())
+    except yaml.YAMLError as e:
+        print >>sys.stderr, "could not read YAML graph data: %s" % (e)
+        return 1
+
+    print "graph {"
+
+    for room, doors in maze.iteritems():
+        print "  r%s -> {%s}" % (room, " ".join(map(lambda door: "r%s" % (door), doors)))
+
+    print "}"
 
 if __name__ == "__main__":
     sys.exit(main())
